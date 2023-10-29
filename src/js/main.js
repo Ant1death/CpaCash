efc27bd39e8136ea1af4e699cca304b3f2f3cc84
Ant1death <Ant1death@mail.ru>
2023-10-29 20:02:38 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,6 @@ Swiper.use([Navigation]);
 window.addEventListener('DOMContentLoaded',() => {
     const companySlider = document.querySelector('.company__wrapper');
     if(companySlider) {
-        // if(window.innerWidth >= 1024) {
-            
-        // }
         const companySlider = new Swiper('.company__wrapper', {
             loop: false,
             direction: 'horizontal',
@@ -38,39 +35,45 @@ window.addEventListener('DOMContentLoaded',() => {
                 },
               },
         });
-        
     }
-    // const offlineSlider = document.querySelector('.offline__wrapper');
-    // if(offlineSlider) {
-    //     if(window.innerWidth >= 1024) {
-    //         const offlineSlider = new Swiper('.offline__wrapper', {
-    //             loop: false,
-    //             direction: 'horizontal',
-    //             allowTouchMove: false,
-    //             draggable: false,
-    //             simulateTouch: false,
-    //             slidesPerView: 3,
-    //             spaceBetween: 24,
-    //             breakpoints: {
-    //                 1200: {
-    //                     loop: false,
-    //                     direction: 'horizontal',
-    //                     allowTouchMove: false,
-    //                     draggable: false,
-    //                     simulateTouch: false,
-    //                     slidesPerView: 3,
-    //                     spaceBetween: 24,
-    //                 },
-    //                 768: {
-    //                     slidesPerView: 2,
-    //                 },
-    //             },
-    //             navigation: {
-    //               nextEl: '.offline__next',
-    //               prevEl: '.offline__prev',
-    //             },
-    //         });
-    //     }
-        
-    // }
+    const bonusesSection = document.querySelector('.bonus');
+    const bonusesLi = document.querySelectorAll('.bonus__wrapper-item ul li');
+    const bonusesImg = document.querySelectorAll('.bonus__wrapper-item img');
+    let isFunctionExecuted = false;
+
+    function handleSectionScroll() {
+      const sectionRect = bonusesSection.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      if (sectionRect.top < windowHeight && sectionRect.bottom >= 0 && !isFunctionExecuted) {
+          isFunctionExecuted = true;
+          let currentIndex = 1;
+          const timer = setInterval(() => {
+              bonusesLi[currentIndex].classList.add('active');
+              bonusesImg[currentIndex].classList.add('active');
+              if (currentIndex > 0) {
+                  bonusesLi[currentIndex - 1].classList.remove('active');
+                  bonusesImg[currentIndex - 1].classList.remove('active');
+              }
+              if (currentIndex === bonusesLi.length - 1) {
+                  clearInterval(timer);
+              }
+              currentIndex++;
+          }, 5000);
+      }
+  }
+  window.addEventListener('scroll', handleSectionScroll);
+
+  // burger
+  const burger = document.querySelector('.burger');
+  const headerMenu = document.querySelector('.header__menu');
+  burger.addEventListener('click',(e) => {
+    burger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    if (burger.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  });
+    
 });
