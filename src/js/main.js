9f98119c8e590105c2b23ea24a3f05960d5d9ccd
Ant1death <Ant1death@mail.ru>
2023-11-19 20:58:05 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -3,57 +3,62 @@ import Swiper from 'swiper';
 import { Navigation} from 'swiper/modules';
 Swiper.use([Navigation]);
 window.addEventListener('DOMContentLoaded',() => {
-    const companySlider = document.querySelector('.company__wrapper');
-    if(companySlider) {
-        const companySlider = new Swiper('.company__wrapper', {
-            loop: false,
-            direction: 'horizontal',
-            allowTouchMove: false,
-            draggable: false,
-            simulateTouch: false,
-            slidesPerView: 3,
-            spaceBetween: 24,
-            navigation: {
-              nextEl: '.company__next',
-              prevEl: '.company__prev',
-            },
-            breakpoints: {
-                769: {
-                    slidesPerView: 3,
-                    slidesPerGroup: 1,
-                    allowTouchMove: false,
-                    draggable: false,
-                    simulateTouch: false,
-                    spaceBetween: 24,
-                  },
-                0: {
-                  slidesPerView: 1,
-                  allowTouchMove: true,
-                  draggable: true,
-                  simulateTouch: true,
-                  spaceBetween: 10,
+  const companySlider = document.querySelector('.company__wrapper');
+  if(companySlider) {
+      const companySlider = new Swiper('.company__wrapper', {
+          loop: false,
+          direction: 'horizontal',
+          allowTouchMove: false,
+          draggable: false,
+          simulateTouch: false,
+          slidesPerView: 3,
+          spaceBetween: 24,
+          navigation: {
+            nextEl: '.company__next',
+            prevEl: '.company__prev',
+          },
+          breakpoints: {
+              769: {
+                  slidesPerView: 3,
+                  slidesPerGroup: 1,
+                  allowTouchMove: false,
+                  draggable: false,
+                  simulateTouch: false,
+                  spaceBetween: 24,
                 },
+              0: {
+                slidesPerView: 1,
+                allowTouchMove: true,
+                draggable: true,
+                simulateTouch: true,
+                spaceBetween: 10,
               },
-        });
-    }
-    const bonusesSection = document.querySelector('.bonus');
-    const bonusesLi = document.querySelectorAll('.bonus__wrapper-item ul li');
-    const bonusesImg = document.querySelectorAll('.bonus__wrapper-item img');
-    let isFunctionExecuted = false;
+            },
+      });
+  }
+  const bonusesSection = document.querySelector('.bonus');
+  const bonusesLi = document.querySelectorAll('.bonus__wrapper-item ul li');
+  const bonusesImg = document.querySelectorAll('.bonus__wrapper-item img');
+  let isFunctionExecuted = false;
+  let currentIndex = 1;
+  let timer;
 
-    function handleSectionScroll() {
+  function handleSectionScroll() {
       const sectionRect = bonusesSection.getBoundingClientRect();
       const windowHeight = window.innerHeight;
+
       if (sectionRect.top < windowHeight && sectionRect.bottom >= 0 && !isFunctionExecuted) {
           isFunctionExecuted = true;
-          let currentIndex = 1;
-          const timer = setInterval(() => {
+
+          timer = setInterval(() => {
               bonusesLi[currentIndex].classList.add('active');
               bonusesImg[currentIndex].classList.add('active');
+
               if (currentIndex > 0) {
-                  bonusesLi[currentIndex - 1].classList.remove('active');
+                  bonusesLi[currentIndex - 1].classList.add('active');
                   bonusesImg[currentIndex - 1].classList.remove('active');
               }
+
               if (currentIndex === bonusesLi.length - 1) {
                   clearInterval(timer);
               }
@@ -61,6 +66,31 @@ window.addEventListener('DOMContentLoaded',() => {
           }, 5000);
       }
   }
+
+  function handleLiClick(index) {
+      clearInterval(timer);
+
+      currentIndex = index;
+
+      bonusesLi.forEach((li, i) => {
+          li.classList.remove('active');
+          if (i <= currentIndex) {
+              li.classList.add('active');
+          }
+      });
+
+      bonusesImg.forEach((img, i) => {
+          img.classList.remove('active');
+          if (i === currentIndex) {
+              img.classList.add('active');
+          }
+      });
+  }
+
+  bonusesLi.forEach((li, index) => {
+      li.addEventListener('click', () => handleLiClick(index));
+  });
+
   window.addEventListener('scroll', handleSectionScroll);
 
   // burger
